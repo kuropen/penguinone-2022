8e170250f508764f05dc0c7a50aeef73424424c9
Hirochika Yuda <hyuda@outlook.jp>
2022-06-14 21:05:15 +0900

    Add some redirect settings

--- next.config.js
+++ next.config.js
@@ -16,7 +16,32 @@ const nextConfig = {
         source: '/ja',
         destination: '/',
         permanent: false,
-      }
+      },
+      {
+        source: '/sitemap/:path*',
+        destination: 'https://sitemap.kuropen.org/sitemap.xml',
+        permanent: true,
+      },
+      {
+        source: '/articles/:path',
+        destination: 'https://pgn-old-blog-url.kuropen.workers.dev/articles/:path',
+        permanent: true,
+      },
+      {
+        source: '/blog/:path',
+        destination: 'https://pgn-old-blog-url.kuropen.workers.dev/blog/:path',
+        permanent: true,
+      },
+      {
+        source: '/ja/posts/:path',
+        destination: '/posts/:path',
+        permanent: true,
+      },
+      {
+        source: '/en/posts/:path',
+        destination: '/posts/:path',
+        permanent: true,
+      },
     ]
   },
   reactStrictMode: true,
